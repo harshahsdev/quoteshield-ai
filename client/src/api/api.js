5804c256cc3c03169f6afc001001fd589932546b
harshahsdev <harsha.dev2025@gmail.com>
2026-07-19 20:55:40 +0530

    cors

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,16 +1,21 @@
 import axios from "axios";
 
+console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
+console.log("MODE =", import.meta.env.MODE);
+
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
-api.interceptors.request.use((config) =>{
-    const token = localStorage.getItem("token");
-    if(token){
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
 
+api.interceptors.request.use((config) => {
+  console.log("Request URL:", config.baseURL + config.url);
+
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
