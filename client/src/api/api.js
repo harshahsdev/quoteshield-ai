0c6050a94ae80a78885d2254c5f72967442bf964
harshahsdev <harsha.dev2025@gmail.com>
2026-07-19 20:09:29 +0530

    cors

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://quoteshield-ai.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 api.interceptors.request.use((config) =>{
