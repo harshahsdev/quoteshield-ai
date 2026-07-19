c02f3f6f5a61e5f7086bf47adaf8ad8e2e19cef5
harshahsdev <harsha.dev2025@gmail.com>
2026-07-19 21:24:56 +0530

    cors

--- server/middleware/multerConfig.js
+++ server/middleware/multerConfig.js
@@ -1,11 +1,21 @@
-import multer from 'multer';
+import multer from "multer";
+import fs from "fs";
+import path from "path";
+
+const uploadPath = path.join(process.cwd(), "uploads");
+
+// Create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, "uploads/");
+    destination: (req, file, cb) => {
+        cb(null, uploadPath);
     },
-    filename: (req, file, cb)=>{
+    filename: (req, file, cb) => {
         cb(null, Date.now() + "-" + file.originalname);
-    }
+    },
 });
+
 export const upload = multer({ storage });
